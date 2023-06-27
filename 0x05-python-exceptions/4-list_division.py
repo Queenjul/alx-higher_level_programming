0053766647ed82e5a821ffcb246be7cde8c6abb2
Queenjul <julianainalegwu@gmail.com>
2023-06-26 18:10:44 -0700

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-
+    '''Divides elements by element 2 lists.'''
     new_list = []
-    index = 0
-    if list_count <= 0:
-        print("out of range")
-        return new_list
-    while index < list_count:
+
+    for index in range(list_length):
         try:
-            new_list.append(my_list_1[index] / my_list_2[index])
+            div = my_list_1[index] / my_list_2[index]
+        except (ValueError, TypeError):
+            print("wrong type")
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
-        except TypeError:
-            print("wrong type")
-            new_list.append(0)
+            div = 0
         except IndexError:
             print("out of range")
-            new_list.append(0)
+            div = 0
         finally:
-            index += 1
-    return new_list
+            new_list.append(div)
+    return (new_list)
