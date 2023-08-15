19fac3233b0e80283b786132516fdeba8a1b6dc1
root <julianainalegwu@gmail.com>
2023-08-15 14:50:27 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
--- Lists the number of records with the same score in the table
--- second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, COUNT(*) AS number
+-- Lists all records of the table second_table of the database
+-- hbtn_0c_0 in your MySQL server.
+SELECT score, name
 FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+WHERE name != ""
+ORDER BY score DESC;
